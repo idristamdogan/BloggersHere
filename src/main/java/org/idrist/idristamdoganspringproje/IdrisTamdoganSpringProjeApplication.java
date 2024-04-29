c079e2cb6c37750540779e37823a4525adc05ee8
ASUS <idristamdogan@gmail.com>
2024-04-29 14:13:29 +0300

    1.gün

---
+++ src/main/java/org/idrist/idristamdoganspringproje/IdrisTamdoganSpringProjeApplication.java
@@ -0,0 +1,13 @@
+package org.idrist.idristamdoganspringproje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdrisTamdoganSpringProjeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdrisTamdoganSpringProjeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/idrist/idristamdoganspringproje/IdrisTamdoganSpringProjeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idrist.idristamdoganspringproje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdrisTamdoganSpringProjeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
